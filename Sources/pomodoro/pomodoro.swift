d4a6906c752e1c1811c09477f873ee82573faf54
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-02-03 11:58:54 +0000

    Add duration properties.

--- Sources/pomodoro/pomodoro.swift
+++ Sources/pomodoro/pomodoro.swift
@@ -1,8 +1,16 @@
 import Foundation
+import ArgumentParser
 
 @main
-struct pomodoro {
-    static func main() {
-        print("Hello, world!")
+@available(macOS 12, iOS 15, visionOS 1, tvOS 15, watchOS 8, *)
+struct pomodoro: AsyncParsableCommand {
+    @Argument(help: "Duration of the pomodoro timer, in minutes.")
+    var focusDuration: TimeInterval = 25.0
+
+    @Option(name: .short, help: "Duration of the resting timer, in minutes.")
+    var restDuration: TimeInterval = 5.0
+
+    mutating func run() async throws {
+        print(focusDuration)
     }
 }
