f4f2b4585ef7c763a0be5a873e3b4ea39fc8afcf
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-03-12 16:56:13 +0000

    Reflect on `Round` updates, and refactor state machine logic.

--- Sources/pomodoro/PomodoroIterator.swift
+++ Sources/pomodoro/PomodoroIterator.swift
@@ -10,59 +10,101 @@ extension pomodoro {
         private let autoAdvance: Bool
 
         /// List of cycles.
-        private var cycles: Queue
+        private var round: Round!
 
-        /// <#Description#>
-        private var currentRound: Round
+        enum State: Codable, Equatable {
+            case notStarted
 
-        /// Elapsed time which will be check against durations.
-        private var elapsedTime: TimeInterval
+            case advancing
 
-        init(autoAdvance: Bool, cycles c: [Cycle]) {
-            self.autoAdvance = autoAdvance
-            cycles = Queue(cycles: c)
-
-            elapsedTime = 0.0
-
-            let cycle = cycles.dequeue()!
-
-            currentRound = Round(cycle: cycle)
+            case finished
         }
 
-        mutating func start() {
-            currentRound.start()
+        /// <#Description#>
+        private var state: State = .notStarted
+
+        /// Elapsed time which will be check against durations.
+        private var elapsedDuration: Duration = 0.0
+
+        init(autoAdvance: Bool) {
+            self.autoAdvance = autoAdvance
+        }
+
+        init(autoAdvance: Bool, cycles: Queue) {
+            self.autoAdvance = autoAdvance
+        }
+
+        init(autoAdvance: Bool, cycles: [Cycle]) {
+            self.autoAdvance = autoAdvance
+        }
+
+        mutating func start(cycles: UInt8, focusDuration: Duration, restDuration: Duration?) {
+            round = Round(
+                cycleCount: cycles,
+                focusDuration: focusDuration,
+                restDuration: focusDuration
+            )
+
+            state = .advancing
+        }
+
+        mutating func start(cycles: Queue) {
+            round = Round(cycles: cycles)
+
+            state = .advancing
+        }
+
+        mutating func start(cycles: [Cycle]) {
+            round = Round(cycles: Queue(cycles))
+
+            state = .advancing
         }
 
         @MainActor
         mutating func advance() -> ShouldContinue {
-            let duration: Duration? = {
-                guard let phase = currentRound.phase else {
-                    return nil
-                }
+            switch state {
+                case .notStarted:
+                    fatalError("Use start first!")
 
-                switch phase {
-                    case .focusing:
-                        return currentRound.cycle.focus
+                case .advancing:
+                    return advance(round: round)
 
-                    case .resting:
-                        return currentRound.cycle.rest
-                }
-            }()
+                case .finished:
+                    startNewRoundIfPossible()
 
-            guard let duration else {
-                return false
+                    return false
+            }
+        }
+
+        @MainActor
+        private mutating func advance(round: Round) -> ShouldContinue {
+            // moving forward
+            elapsedDuration += 1
+
+            let horizon: Duration
+
+            switch round.phase.state {
+                case .focusing:
+                    horizon = round.phase.cycle.focus
+
+                case .resting:
+                    if let restingDuration = round.phase.cycle.rest {
+                        horizon = restingDuration
+                    } else {
+                        // start new cycle
+                        // goto new focusing if possible
+
+                        return false
+                    }
             }
 
-            // moving forward
-            elapsedTime += 1
-
-            ConsoleOutput.printLoading(for: elapsedTime, horizon: duration)
+            ConsoleOutput.printLoading(for: elapsedDuration, horizon: horizon)
 
             // book keeping
             let isCounterPassedHorizon: Bool = {
-                let horizonDuration = duration * 60
+                let horizonDuration = horizon * 60
 
-                return elapsedTime >= horizonDuration
+                return elapsedDuration >= horizonDuration
             }()
 
             guard isCounterPassedHorizon else {
@@ -70,8 +112,8 @@ extension pomodoro {
                 return true
             }
 
-            let _currentRound = currentRound
-            announceEndOfThePhase(for: _currentRound)
+            let currentPhase = round.phase.state
+            announceEndOfThePhase(currentPhase)
 
             if autoAdvance {
                 return moveForward()
@@ -84,18 +126,15 @@ extension pomodoro {
             return moveForward()
         }
 
-        private func announceEndOfThePhase(for round: Round) {
+        private func announceEndOfThePhase(_ state: Phase.State) {
             let confirmationMessage: String
 
-            switch round.phase {
+            switch state {
                 case .focusing:
                     confirmationMessage = "Lets take a break! 🎉"
 
                 case .resting:
                     confirmationMessage = "Back to work!"
-
-                default:
-                    fatalError("Asking user to continue in an invalid state.")
             }
 
             NotificationProxy.notify(title: "Pomodoro", message: confirmationMessage)
@@ -131,50 +170,51 @@ extension pomodoro {
         private mutating func moveForward() -> ShouldContinue {
             let message: String
 
-            switch currentRound.phase {
-                case .focusing:
-                    if let restDuration = currentRound.cycle.rest {
-                        message = "Starting rest phase for '\(restDuration)' minutes."
+            switch state {
+                case .notStarted:
+                    fatalError()
 
-                        // advance phase
-                        currentRound.switchToRest()
-                    } else {
-                        // move immediatly to next cycle.
-                        let cycle = startNewRound()
+                case .advancing:
+                    do {
+                        let currentPhase = round.phase.state
 
-                        message = "Starting a new focus cycle for '\(cycle.focus)' minutes"
+                        try round.moveForward()
+
+                        switch currentPhase {
+                            case .focusing:
+                                if let restDuration = round.phase.cycle.rest {
+                                    message = "Starting rest phase for '\(restDuration)' minutes."
+                                } else {
+                                    message = "Starting a new focus cycle without resting for '\(round.phase.cycle.focus)' minutes"
+                                }
+
+                            case .resting:
+                                message = "Starting a new focus cycle for '\(round.phase.cycle.focus)' minutes"
+                        }
+                    } catch {
+                        message = "Starting a new round!"
+
+                        startNewRoundIfPossible()
                     }
 
-                case .resting:
-                    let cycle = startNewRound()
+                case .finished:
+                    message = "Starting a new round!"
 
-                    message = "Starting a new focus cycle for '\(cycle.focus)' minutes"
-
-                default:
-                    return false
+                    startNewRoundIfPossible()
             }
 
-            elapsedTime = 0
+            elapsedDuration = 0
 
             print(message)
 
             return true
         }
 
-        private mutating func startNewRound() -> Cycle {
-            // replace phase with a new one
-            // aka start fresh
-            guard let cycle = cycles.dequeue() else {
-                fatalError()
-            }
-
-            let newPhase = Round(cycle: cycle)
-
-            currentRound = newPhase
-
-            currentRound.start()
-
-            return cycle
+        private mutating func startNewRoundIfPossible() {
+//            let newRound = Round(cycles: 4)
+//
+//            return newRound
+//            return nil
         }
     }
 }
