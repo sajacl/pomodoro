4853423e4bec72a9ff3945349cb36e6e4ee6c65e
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-03-14 17:45:48 +0000

    Adopt changes in `Round`.

--- Sources/pomodoro/PomodoroIterator.swift
+++ Sources/pomodoro/PomodoroIterator.swift
@@ -3,34 +3,64 @@ import Foundation
 /// Allowed character that will move state forward.
 private let continuationCharacters: Set<Character> = ["Y"]
 
-extension pomodoro {
+extension Pomodoro {
     /// <#Description#>
-    struct Iterator: Codable, Equatable {
+    final class Iterator: Codable, Equatable {
         /// Boolean flag that indicates wheter the app should advance without asking.
         private let autoAdvance: Bool
+
+        private let display: @MainActor (String) -> Void
+
+        private let displayLoading: @MainActor (_ for: Duration, _ horizon: Duration) -> Void
+
+        private let notify: @MainActor (String) -> Void
 
         /// List of cycles.
         private var round: Round!
 
-        enum State: Codable, Equatable {
-            case notStarted
-
-            case advancing
-
-            case finished
-        }
-
-        /// <#Description#>
-        private var state: State = .notStarted
-
         /// Elapsed time which will be check against durations.
         private var elapsedDuration: Duration = 0.0
 
-        init(autoAdvance: Bool) {
+        init(
+            autoAdvance: Bool,
+            display: @escaping @MainActor (String) -> Void = { ConsoleOutput.print($0) },
+            displayLoading: @escaping @MainActor (Duration, Duration) -> Void = { ConsoleOutput.printLoading(for: $0, horizon: $1) },
+            notify: @escaping @MainActor (String) -> Void = { NotificationProxy.notify(message: $0) }
+        ) {
             self.autoAdvance = autoAdvance
+            self.display = display
+            self.displayLoading = displayLoading
+            self.notify = notify
         }
 
-        mutating func start(
+        enum CodingKeys: String, CodingKey {
+            case autoAdvance
+
+            case round
+
+            case elapsedDuration
+        }
+
+        // Decodable
+        required init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.autoAdvance = try container.decode(Bool.self, forKey: .autoAdvance)
+            self.round = try container.decodeIfPresent(Round.self, forKey: .round)
+            self.elapsedDuration = try container.decode(Duration.self, forKey: .elapsedDuration)
+
+            // restore our default closures after decoding
+            self.display = { ConsoleOutput.print($0) }
+            self.displayLoading = { ConsoleOutput.printLoading(for: $0, horizon: $1) }
+            self.notify = { NotificationProxy.notify(message: $0) }
+        }
+
+        static func == (lhs: Iterator, rhs: Iterator) -> Bool {
+            lhs.autoAdvance == rhs.autoAdvance &&
+            lhs.round == rhs.round &&
+            lhs.elapsedDuration == rhs.elapsedDuration
+        }
+
+        func start(
             cycles: UInt8,
             with focusDuration: Duration,
             and restDuration: Duration?
@@ -40,114 +70,76 @@ extension pomodoro {
                 focusDuration: focusDuration,
                 restDuration: focusDuration
             )
-
-            state = .advancing
         }
 
-        mutating func start(with cycles: Queue) {
+        func start(with cycles: Queue) {
             round = Round(cycles: cycles)
-
-            state = .advancing
         }
 
-        mutating func start(with cycles: [Cycle]) {
+        func start(with cycles: [Cycle]) {
             round = Round(cycles: Queue(cycles))
-
-            state = .advancing
         }
 
         @MainActor
-        mutating func advance() -> ShouldContinue {
-            switch state {
-                case .notStarted:
-                    fatalError("Use start first!")
-
-                case .advancing:
-                    return advance(round: round)
-
-                case .finished:
-                    startNewRoundIfPossible()
-
-                    return false
-            }
-        }
-
-        @MainActor
-        private mutating func advance(round: Round) -> ShouldContinue {
+        func advance() throws {
             // moving forward
             elapsedDuration += 1
 
-            let horizon: Duration
+            let horizon: Duration = round.cycle.phase.duration
 
-            switch round.phase.state {
-                case .focusing:
-                    horizon = round.phase.cycle.focus
+            displayLoading(elapsedDuration, horizon)
 
-                case .resting:
-                    if let restingDuration = round.phase.cycle.rest {
-                        horizon = restingDuration
-                    } else {
-                        // start new cycle
-                        // goto new focusing if possible
+            // counter book keeping
 
-                        return false
-                    }
-            }
-
-            ConsoleOutput.printLoading(for: elapsedDuration, horizon: horizon)
-
-            // book keeping
             let isCounterPassedHorizon: Bool = {
                 let horizonDuration = horizon * 60
 
                 return elapsedDuration >= horizonDuration
             }()
 
-            guard isCounterPassedHorizon else {
+            if !isCounterPassedHorizon {
                 // time has not passed yet
-                return true
+                return
             }
 
-            let currentPhase = round.phase.state
-            announceEndOfThePhase(currentPhase)
+            // end of the current phase
+            announceEndOfPhase()
 
-            if autoAdvance {
-                return moveForward()
+            if autoAdvance || askUserForContinuation() {
+                try moveForward()
+            } else {
+                throw CancellationError()
             }
-
-            guard askUserForContinuation() else {
-                return false
-            }
-
-            return moveForward()
         }
 
-        private func announceEndOfThePhase(_ state: Phase.State) {
+        @MainActor
+        private func announceEndOfPhase() {
             let confirmationMessage: String
 
-            switch state {
-                case .focusing:
+            switch round.cycle.phase {
+                case .focused:
                     confirmationMessage = "Lets take a break! 🎉"
 
                 case .resting:
-                    confirmationMessage = "Back to work!"
+                    confirmationMessage = "Work does not stop, grind does not stop! 💻"
             }
 
-            NotificationProxy.notify(title: "Pomodoro", message: confirmationMessage)
+            notify(confirmationMessage)
 
-            print("\n")
-            print(confirmationMessage)
+            display("\n")
+            display(confirmationMessage)
         }
 
+        @MainActor
         private func askUserForContinuation() -> ShouldContinue {
             let continuationCharactersDescription = continuationCharacters
                 .map { "'\($0)'" }
                 .joined(separator: "|")
 
-            print("Press \(continuationCharactersDescription) to continue.")
+            display("Press \(continuationCharactersDescription) to continue.")
 
             // ask for continuation
-            let character = readLine()
+            let character = readLine()?.first
 
             let shouldContinue = {
                 guard let character else {
@@ -163,83 +155,38 @@ extension pomodoro {
             return shouldContinue
         }
 
-        private mutating func moveForward() -> ShouldContinue {
-            let currentPhase = round.phase
+        @MainActor
+        private func moveForward() throws {
+            let currentPhase = round.cycle.phase
 
-            let couldAdvanceCurrentPhase: Bool
+            try round.moveForward()
 
-            switch state {
-                case .notStarted:
-                    fatalError()
+            announceNewPhase(basedOn: currentPhase)
 
-                case .advancing:
-                    do {
-                        try round.moveForward()
-
-                        couldAdvanceCurrentPhase = true
-                    } catch {
-                        startNewRoundIfPossible()
-
-                        couldAdvanceCurrentPhase = false
-                    }
-
-                case .finished:
-                    startNewRoundIfPossible()
-
-                    couldAdvanceCurrentPhase = false
-            }
-
-            announceCompletion(
-                previousState: state,
-                currentPhase: currentPhase.state,
-                couldAdvanceCurrentPhase: couldAdvanceCurrentPhase
-            )
-
-            elapsedDuration = 0
-
-            return true
+            elapsedDuration = 0.0
         }
 
-        private func announceCompletion(
-            previousState: State,
-            currentPhase: Phase.State,
-            couldAdvanceCurrentPhase: Bool
-        ) {
+        @MainActor
+        private func announceNewPhase(basedOn oldPhase: Phase) {
             let message: String
 
-            switch state {
-                case .notStarted:
-                    fatalError()
+            switch oldPhase {
+                case let .focused(duration):
+                    message = "[\(round.cycle.index)] Starting rest phase for '\(duration)' minutes."
 
-                case .advancing:
-                    if couldAdvanceCurrentPhase {
-                        switch currentPhase {
-                            case .focusing:
-                                if let restDuration = round.phase.cycle.rest {
-                                    message = "Starting rest phase for '\(restDuration)' minutes."
-                                } else {
-                                    message = "Starting a new focus cycle without resting for '\(round.phase.cycle.focus)' minutes"
-                                }
-
-                            case .resting:
-                                message = "Starting a new focus cycle for '\(round.phase.cycle.focus)' minutes"
-                        }
-                    } else {
-                        message = "Starting a new round!"
-                    }
-
-                case .finished:
-                    message = "Starting a new round!"
+                case let .resting(duration):
+                    message = "[\(round.cycle.index)] Starting a new focus cycle for '\(duration)' minutes."
             }
 
-            print(message)
+            display(message)
         }
 
-        private mutating func startNewRoundIfPossible() {
-//            let newRound = Round(cycles: 4)
-//
-//            return newRound
-//            return nil
+        // Encodable
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(autoAdvance, forKey: .autoAdvance)
+            try container.encodeIfPresent(round, forKey: .round)
+            try container.encode(elapsedDuration, forKey: .elapsedDuration)
         }
     }
 }
