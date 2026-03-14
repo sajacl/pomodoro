62fff80a9aded9fc3a5f8845eafc5ef250a92aca
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-03-14 20:07:50 +0000

    Remove rest duration being optinal.

--- Sources/pomodoro/Cycle.swift
+++ Sources/pomodoro/Cycle.swift
@@ -6,29 +6,5 @@ struct Cycle: Codable, Equatable {
     var focus: Duration
 
     /// Optional duration (in minutes) for resting.
-    var rest: Duration?
-}
-
-extension [Cycle] {
-    /// Creates a list of cycles with a specified focus, rest duration, and count.
-    /// The last cycle has a longer rest period.
-    static func create(
-        focusDuration: Duration,
-        restDuration: Duration,
-        cycleCount: UInt8
-    ) -> [Cycle] {
-        var cycles: [Cycle] = []
-        cycles.reserveCapacity(Int(cycleCount))
-
-        for _ in 0..<(cycleCount - 1) {
-            cycles.append(
-                Cycle(focus: focusDuration, rest: restDuration)
-            )
-        }
-
-        // contains longer resting cycle
-        cycles.append(Cycle(focus: focusDuration, rest: 7.5 * Double(cycleCount)))
-
-        return cycles
-    }
+    var rest: Duration
 }
