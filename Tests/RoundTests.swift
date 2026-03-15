601fca430c8d9a9b37cd2d3d6835066134351651
Sajad Vishkai <sajaclvishkai@gmail.com>
2026-03-15 16:44:42 +0000

    Add `creation_nil` test method.

--- Tests/RoundTests.swift
+++ Tests/RoundTests.swift
@@ -2,5 +2,11 @@ import Testing
 @testable import pomodoro
 
 struct RoundTests {
+    @Test
+    func creation_nil() {
+        let emptyCycles = Queue()
+        let round = Round(cycles: emptyCycles)
 
+        #expect(round == nil)
+    }
 }
